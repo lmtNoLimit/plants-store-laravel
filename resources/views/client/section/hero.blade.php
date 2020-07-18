018facb681adc2b9fc9f16a0597e54ab4ebba741
Lam Thanh <lmt151099@gmail.com>
2020-07-18 10:35:34 +0700

    only homepage has banner

--- resources/views/client/section/hero.blade.php
+++ resources/views/client/section/hero.blade.php
@@ -44,16 +44,6 @@
             </div>
           </div>
         </div>
-        @if(Route::current()->getName() == 'homepage')
-        <div class="hero__item set-bg" data-setbg="img/hero/banner.jpg">
-          <div class="hero__text">
-            <span>FRUIT FRESH</span>
-            <h2>Vegetable <br />100% Organic</h2>
-            <p>Free Pickup and Delivery Available</p>
-            <a href="#" class="primary-btn">SHOP NOW</a>
-          </div>
-        </div>
-        @endif
       </div>
     </div>
   </div>
